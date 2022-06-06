2f698baec8992efcceb8d40de30b3569e93f37e1
frank <frank96haver@gmail.com>
2022-06-06 13:52:40 +0200

    Use gulp

---
+++ VVSkunk.Html/gulpfile.js
@@ -0,0 +1,45 @@
+const { src, dest, series, watch } = require('gulp')
+const del = require('del')
+const njk = require('gulp-nunjucks-render')
+const beautify = require('gulp-beautify')
+const sass = require('gulp-sass')(require('sass'));
+const minify = require('gulp-minify');
+
+function buildStyles() {
+    return src('./src/css/*.css')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(dest('./dist/css'));
+};
+
+function jsCompress() {
+    return src(['src/js/*.js'])
+        .pipe(minify())
+        .pipe(dest('dist/js'))
+}
+
+function moveFiles() {
+    return src(['public/**'])
+        .pipe(dest('dist'))
+}
+
+function clean() {
+    return del(['dist'])
+}
+
+function html() {
+    return src('src/html/pages/*.+(html|njk)')
+        .pipe(
+            njk({
+                path: ['src/html', 'src/macros'],
+            })
+        )
+        .pipe(beautify.html({ indent_size: 4, preserve_newlines: false }))
+        .pipe(dest('dist'))
+}
+
+function watchFiles() {
+    watch('src/html/**/*', html)
+}
+
+exports.build = series(clean, html, buildStyles, jsCompress, moveFiles)
+exports.default = series(clean, html, watchFiles)
